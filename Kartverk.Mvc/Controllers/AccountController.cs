2abd7a1918ff8b6191938672c728663669b5de2d
anna oestmoen <annamoe03@hotmail.com>
2024-09-27 13:44:27 +0200

    La til Logg inn side

---
+++ Kartverk.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    // GET: Account/LoggInn
+    public ActionResult LoggInn()
+    {
+        return View();
+    }
+
+    // For post request handling of login
+    [HttpPost]
+    public ActionResult LoggInn(LogginnViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Handle login logic here, e.g., verify user credentials
+            // Redirect to another page if successful
+        }
+
+        // Return the view with validation messages if login fails
+        return View(model);
+    }
+}
