08bb6d0953931b83a10288849832d1f025d58fa2
anna oestmoen <annamoe03@hotmail.com>
2024-10-01 09:56:12 +0200

    La til admin side

--- Kartverk.Mvc/Controllers/MinSide/MinSideController.cs
+++ Kartverk.Mvc/Controllers/MinSide/MinSideController.cs
@@ -27,4 +27,33 @@ public class MinSideController : Controller
         // Hvis brukeren ikke finnes, returner til LoggInn
         return RedirectToAction("LoggInn", "Account");
     }
+
+    // GET: MinSide/AdminLogin
+    [HttpGet]
+    public IActionResult AdminLogin()
+    {
+        return View();
+    }
+
+    // POST: MinSide/AdminLogin
+    [HttpPost]
+    public IActionResult AdminLogin(string adminPassword)
+    {
+        const string predefinedPassword = "admin123"; // Predefinert passord for admin
+        if (adminPassword == predefinedPassword)
+        {
+            // Hvis passordet er riktig, omdiriger til admin-siden
+            return RedirectToAction("AdminDashboard");
+        }
+
+        // Hvis passordet er feil, vis feilmelding
+        ModelState.AddModelError("", "Feil admin-passord.");
+        return View();
+    }
+
+    // GET: MinSide/AdminDashboard
+    public IActionResult AdminDashboard()
+    {
+        return View(); // Dette blir admin-siden
+    }
 }
