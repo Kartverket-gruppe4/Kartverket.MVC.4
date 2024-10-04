31789fddbfcf61109a63d89b0665e907d1795c6e
SveleThea <svelethea@gmail.com>
2024-10-04 11:40:26 +0200

    fikset min egen feil

--- Kartverk.Mvc/Models/Account/IdentityUser.cs
+++ Kartverk.Mvc/Models/Account/IdentityUser.cs
@@ -1,6 +1,6 @@
 public class IdentityUser
 {
     public string? UserName { get; set; }
-    public string? Epost { get; set; }
-    public string? Passord { get; set; } // Nytt felt for � lagre passordet
+    public string? Email { get; set; }
+    public string? Password { get; set; } // Nytt felt for � lagre passordet
 }
