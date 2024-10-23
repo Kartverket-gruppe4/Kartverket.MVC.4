22a0a99024e2f777d02273c6aea755d40728442a
anna oestmoen <annamoe03@hotmail.com>
2024-10-23 20:28:31 +0200

    La til skjema på admin side, ikke helt fullstendig ferdig.

--- Kartverk.Mvc/Controllers/AdminFeilmelding/AdminSideController.cs
+++ Kartverk.Mvc/Controllers/AdminFeilmelding/AdminSideController.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using Kartverk.Mvc.Models.Feilmelding;
+using System.Collections.Generic;
+using Kartverk.Mvc.Controllers.FeilMelding;
 
 namespace Kartverk.Mvc.Controllers.AdminFeilmelding
 {
     public class AdminFeilmeldingController : Controller
     {
+        // GET: AdminFeilmelding/Index
         public IActionResult Index()
         {
-            return View(); // Pek på "Index" visningen i Views/AdminFeilmelding
+            // Hent alle feilmeldinger fra tjenesten eller databasen
+            var feilmeldinger = FeilmeldingController._feilmeldinger;
+
+            // Send listen av feilmeldinger til visningen
+            return View(feilmeldinger);
         }
     }
 }
